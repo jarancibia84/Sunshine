821401874cccf19104635dd589ca93d51c8f6fae
Jaime <jarancibia84@hotmail.com>
2015-05-01 02:22:27 -0300

    3.0_share_intent

--- app/src/main/java/com/example/livlab/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/livlab/sunshine/app/DetailActivity.java
@@ -1,27 +1,31 @@
+
 package com.example.livlab.sunshine.app;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 
+
 public class DetailActivity extends ActionBarActivity {
 
-
+private ShareActionProvider mSharedActionProvider;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_detail); //ELIMINAR SEGUN DOCUMENTACION
-
-
-
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -34,11 +38,11 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
+      //  getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
-    @Override
+     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -57,19 +61,49 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public class DetailFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
+
+        private final static String LOG_TAG = DetailFragment.class.getSimpleName();
+        private final static String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+        private String forecastString;
 
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+            inflater.inflate(R.menu.detailfragment,menu);
+            MenuItem item = menu.findItem(R.id.action_share);
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if(shareActionProvider != null)
+                shareActionProvider.setShareIntent(createShareIntent());
+            else
+                Log.d("shareActionProvider", "ShareActionProvidr is null");
+
+
+            super.onCreateOptionsMenu(menu, inflater);
+        }
+
+        private Intent createShareIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    forecastString +FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-            Intent intent = getIntent();
+            Intent intent = getActivity().getIntent();
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastString = intent.getExtras().getString(Intent.EXTRA_TEXT);
+                forecastString = intent.getExtras().getString(Intent.EXTRA_TEXT);
                 TextView texto = (TextView) rootView.findViewById(R.id.detail_text);
                 texto.setText(forecastString);
             }
